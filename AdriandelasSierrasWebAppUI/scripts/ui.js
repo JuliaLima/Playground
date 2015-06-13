9583b2209ff01979af43e1ad64a371e9020097d8
JuliaLima <julia.lima.dg@gmail.com>
2015-06-13 14:39:04 -0300

    refactoring in mobile resolution. Need to improve the code

--- AdriandelasSierrasWebAppUI/scripts/ui.js
+++ AdriandelasSierrasWebAppUI/scripts/ui.js
@@ -1,24 +1,130 @@
-var setUiEvents = function() {
-	var bell=$('#bell');
+var hideClass = 'nonDisplayedAll';
 
-	var bellClickBehaviour=function(){
-		var main=$('#main-buttons');
-		var mainDisplayValue = main.css('display');
-		var aside=$('#mainNotifications');
-		if(mainDisplayValue == 'flex'){
-			main.removeClass('displayed');
-			main.addClass('non-displayed');
-			aside.removeClass('non-displayed');
-			aside.addClass('displayed');
+var hideElement=function(elem){
+	elem.addClass(hideClass);
+}
+
+var showElement=function(elem){
+	elem.removeClass(hideClass);
+}
+
+/*var toggle=function(elem){
+	if(){
+	
+	}
+}*/
+
+var setUiEvents = function() {
+	var actions=$('[data-hamburgerIcon]');
+	var actionsClickBehaviour=function(){
+		var modal=$('[data-mainModal]');
+		var modalDisplayValue = modal.css('display');
+		if(modalDisplayValue == 'none'){
+			showElement(modal);
+			$(modal).addClass('animated fadeIn');
 		} else {
-			main.removeClass('non-displayed');
-			main.addClass('displayed');
-			aside.removeClass('displayed');
-			aside.addClass('non-displayed');
+			$(modal).removeClass('animated fadeIn');
+			hideElement(modal);
 		}
 	}
+	actions.click(actionsClickBehaviour);
 	
-	bell.click(bellClickBehaviour);	
+	var cog=$('[data-cog]');
+	var cogClickBehaviour=function(){
+		var modal=$('[data-notif-modal]');
+		var modalDisplayValue = modal.css('display');
+		if(modalDisplayValue == 'none'){
+			showElement(modal);
+			$(modal).addClass('animated fadeIn');
+		} else {
+			$(modal).removeClass('animated fadeIn');
+			hideElement(modal);
+		}
+	}
+	cog.click(cogClickBehaviour);
+	
+	
+	var search=$('[data-search]');
+	var searchClickBehaviour=function(){
+		var modal=$('[data-search-modal]');
+		var modalDisplayValue=modal.css('display');
+		if(modalDisplayValue == 'none'){
+			showElement(modal);
+			$(modal).addClass('animated fadeIn');
+		} else {
+			$(modal).removeClass('animated fadeIn');
+			hideElement(modal);
+		}
+	}
+	search.click(searchClickBehaviour);
+	
+	var userButton=$('[data-userButton]');
+	var userButtonClickBehaviour=function(){
+		var modal=$('[data-user-modal]');
+		var modalDisplayValue=modal.css('display');
+		if(modalDisplayValue == 'none'){
+			showElement(modal);
+			$(modal).addClass('animated fadeIn');
+		} else {
+			$(modal).removeClass('animated fadeIn');
+			hideElement(modal);
+		}
+	}
+	userButton.click(userButtonClickBehaviour);
+	
+	var user=$('[data-user]');
+	var userClickBehaviour=function(){
+		var modal=$('[data-user-modal]');
+		var modalDisplayValue=modal.css('display');
+		if(modalDisplayValue == 'none'){
+			showElement(modal);
+			$(modal).addClass('animated fadeIn');
+		} else {
+			$(modal).removeClass('animated fadeIn');
+			hideElement(modal);			
+		}
+	}
+	user.click(userClickBehaviour);
+	
+	var notif=$('[data-notif]');
+	var notifClickBehaviour=function(){
+		var modal=$('[data-notif-modal]');
+		var modalDisplayValue=modal.css('display');
+		if(modalDisplayValue == 'none'){
+			showElement(modal);
+			$(modal).addClass('animated fadeIn');
+		} else {
+			$(modal).removeClass('animated fadeIn');
+			hideElement(modal);			
+		}
+	}
+	notif.click(notifClickBehaviour);
+
+	
+	var icon=$('[data-notificationsEmail]');
+	var iconClickBehaviour=function(){
+		var buttons=$('[data-iconButtonMessage]');
+		var buttonsDisplayValue = buttons.css('display');
+		if(buttonsDisplayValue == 'none'){
+			$(buttons).removeClass('nonDisplayedAll');
+		} else {
+			$(buttons).addClass('nonDisplayedAll');
+		}
+	}
+	icon.click(iconClickBehaviour);
+	
+	
+	var icon=$('[data-notificationsBus]');
+	var iconClickBehaviour=function(){
+		var buttons=$('[data-iconButtonTicket]');
+		var buttonsDisplayValue = buttons.css('display');
+		if(buttonsDisplayValue == 'none'){
+			$(buttons).removeClass('nonDisplayedAll');
+		} else {
+			$(buttons).addClass('nonDisplayedAll');
+		}
+	}
+	icon.click(iconClickBehaviour);
 };
 
 var body=$('body');
