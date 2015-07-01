37cceb0a9bac0e01faa4458094cf13722731f627
JuliaLima <julia.lima.dg@gmail.com>
2015-07-01 14:53:01 -0300

    articles

--- AdriandelasSierrasWebAppUI/scripts/ui.js
+++ AdriandelasSierrasWebAppUI/scripts/ui.js
@@ -1,86 +1,92 @@
-var classforHiddingFirst = 'non-displayed';
-var openBubbles = {
-	push: function(name){
-		this.popAll();
-		this[name] = true;
-		var bubbleElem = $('[bubble="' + name + '"]');
-		showElement(bubbleElem, 'animated fadeIn', 'animated fadeOut');
-	},
-	pop: function(name){
-		delete this[name];
-		var bubbleElem = $('[bubble="' + name + '"]');
-		hideElement(bubbleElem, 'animated fadeIn', 'animated fadeOut');
-	},
-	popAll: function() {
-		for(var index in this){
-			if(this.hasOwnProperty(index) && index !== 'push' && index !== 'pop' && index !== 'popAll' && index !== 'isOpened' && index !== 'toggle'){
-				this.pop(index);
+var modal = (function(){
+	var opened;
+	var modalTypes = {}
+	
+	return {
+		addType: function(tName, typeObj) {
+			modalTypes[tName] = typeObj;
+		},
+		toggle: function(elem, type) {
+			var isSame = this.closePrev(elem);
+			if(!isSame) {
+				modalTypes[type].open(elem);
+				opened = {
+					type: type,
+					elem: elem
+				};
 			}
+		},
+		closePrev: function(actual) {
+			isSame = false;
+			if(typeof opened === 'object') {
+				modalTypes[opened.type].close(opened.elem);
+				isSame = ((typeof actual === 'object') ? (actual[0] === opened.elem[0]) : false);
+			}
+			opened = undefined;
+			
+			return isSame;;
 		}
-	},
-	toggle: function(name) {
-		if(this.isOpened(name)){
-			this.pop(name);
-		} else {
-			this.push(name);
-		}
-	},
-	isOpened: function(name){
-		return this.hasOwnProperty(name);
 	}
-};
+})();
 
-var showElement = function(elem, animatedShowClassIn, animatedShowClassOut) {
-	elem.removeClass(classforHiddingFirst);
-	elem.removeClass(animatedShowClassOut);
-	elem.addClass(animatedShowClassIn);
-};
+modal.addType('bubble', {
+	open: function(elem) {
+		elem.removeClass('non-displayed');
+		elem.removeClass('animated fadeOut');
+		elem.addClass('animated fadeIn');
+	},
+	close: function(elem){
+		elem.removeClass('animated fadeIn');
+		elem.addClass('animated fadeOut');
+		$(elem).on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+			$(this).off('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
+			$(this).addClass('non-displayed');
+		});
+	}
+});
 
-var hideElement = function(elem, animatedShowClassIn, animatedShowClassOut) {
-	elem.removeClass(animatedShowClassIn);
-	elem.addClass(animatedShowClassOut);
-	$(elem).on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-		$(this).off('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
-		$(this).addClass(classforHiddingFirst);
-	});
-};
-
-var isShown = function(elem, animatedShowClassOut) {
-	return elem.hasClass(classforHiddingFirst) || elem.hasClass(animatedShowClassOut);
-};
+modal.addType('options-menu', {
+	open: function(elem) {
+		var dropdownMenu = elem.children(".dropdown-menu");
+		dropdownMenu.removeClass('non-displayed');
+		dropdownMenu.removeClass('animated flipOutX');
+		dropdownMenu.addClass('animated flipInX');
+		elem.addClass('active');
+	},
+	close: function(elem){
+		var dropdownMenu = elem.children(".dropdown-menu");
+		dropdownMenu.removeClass('animated flipInX');
+		dropdownMenu.addClass('animated flipOutX');
+		elem.removeClass('active');
+		$(dropdownMenu).on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+			dropdownMenu.addClass('non-displayed');
+			//elem.removeClass('active');
+			$(this).off('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
+		});
+	}
+});
 
 var setUiEvents = function() {
 	var bubblesTriggers = $('[bubble-trigger]');
-	var bubblesTriggersClickBehaviour = function() {
+	var bubblesTriggersClickBehaviour = function(e) {
+		e.stopPropagation();
 		var bubbleName = $(this).attr('bubble-trigger');
-		openBubbles.toggle(bubbleName);
+		var bubbleElem = $('[bubble="' + bubbleName + '"]');
+		modal.toggle(bubbleElem, 'bubble');
 	};
 	bubblesTriggers.click(bubblesTriggersClickBehaviour);
 	
-	/*var main = $('main, aside nav');
+	var body = $('body');
 	var clickOnBody = function() {
-		openBubbles.popAll();
+		modal.closePrev();
 	};
-	main.click(clickOnBody);*/
+	body.click(clickOnBody);
 	
 	var optionsMenu = $('[options-menu] ul li');
-	var optionsMenuClickBehaviour = function() {
+	var optionsMenuClickBehaviour = function(e) {
+		e.stopPropagation();
 		var li = $(this);
-		var dropdownMenu = li.children(".dropdown-menu");
-		if(dropdownMenu.hasClass('non-displayed')) {
-			dropdownMenu.removeClass('non-displayed');
-			dropdownMenu.removeClass('animated flipOutX');
-			dropdownMenu.addClass('animated flipInX');
-			li.addClass('active');
-		} else {
-			dropdownMenu.removeClass('animated flipInX');
-			dropdownMenu.addClass('animated flipOutX');
-			$(dropdownMenu).on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-				dropdownMenu.addClass('non-displayed');
-				li.removeClass('active');
-				$(this).off('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
-			});
-		}
+		modal.toggle(li, 'options-menu');
 	};
 	optionsMenu.click(optionsMenuClickBehaviour);
 	
@@ -89,12 +95,20 @@ var setUiEvents = function() {
 		var backElem = $('[twoFacedElement=' + $(this).attr('flip-trigger') + '] [face]').filter(function() {
 			return this.attributes.face.value.match(/isOnTheBack:*/);
 		});
+		/*frontElem.css('height', '100px');
+		frontElem.css('overflow', 'hidden');
+		backElem.css('height', '100px');
+		backElem.css('overflow', 'hidden');*/
 		frontElem.attr('face', 'isOnTheBack:' + (backElem.attr('face').split(':')[1] === '+' ? '-' : '+'));
 		frontElem.on("webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend",
 			function(event) {
 				$(this).off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
 				$(this).css('display', 'none');
+				/*$(this).css('height', '');
+				$(this).css('overflow', '');*/
 				backElem.css('display', '');
+				/*backElem.css('height', '');
+				backElem.css('overflow', '');*/
 				setTimeout(function() {
 					backElem.attr('face', 'isOnTheFront');
 				}, 0);
